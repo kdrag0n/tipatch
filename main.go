cbb3fb4cc11efa58090d4a5cbad317d78302a592
kdrag0n <dragon@khronodragon.com>
2018-07-12 21:23:17 -0700

    Use bytereplacer instead of bytes.Replace for patching
    bytes.Replace takes about 100ms, including GC overhead.
    bytereplacer.Replacer is extremely fast, close to the speed of no
    replacing at all.
    
    The syntax is also cleaner.

--- main.go
+++ main.go
@@ -11,17 +11,13 @@ import (
 
 	gzip "github.com/klauspost/pgzip"
 	flag "github.com/spf13/pflag"
+	"go4.org/bytereplacer"
 )
 
 // General command-line inteface constants
 const (
 	LastTestedVersion = "3.2.2"
 )
-
-func replace(in *[]byte, from, to string) {
-	replaced := bytes.Replace(*in, []byte(from), []byte(to), -1)
-	*in = replaced
-}
 
 func checkMsg(err error, msg string) {
 	if err != nil {
@@ -110,12 +106,16 @@ Last tested with TWRP %s
 	checkMsg(err, "cleaning up ramdisk extraction")
 
 	fmt.Println(" - Patching ramdisk")
-	// Preserve /data/media
-	replace(&ramdisk, "\x00/media\x00", "\x00/.twrp\x00")
-	// Change text in Backup screen for English
-	replace(&ramdisk, "Data (excl. storage)", "Data (incl. storage)")
-	// Change orange warning text when backing up for English
-	replace(&ramdisk, "Backups of {1} do not include any files in internal storage such as pictures or downloads.", "Backups of {1} include files in internal storage such as pictures and downloads.          ")
+	replacer := bytereplacer.New(
+		// Preserve /data/media
+		"\x00/media\x00", "\x00/.twrp\x00",
+		// Change text in Backup screen for English
+		"Data (excl. storage)", "Data (incl. storage)",
+		// Change orange warning text when backing up for English
+		"Backups of {1} do not include any files in internal storage such as pictures or downloads.", "Backups of {1} include files in internal storage such as pictures and downloads.          ",
+	)
+
+	ramdisk = replacer.Replace(ramdisk)
 
 	fmt.Println(" - Compressing ramdisk")
 	var gzRamdisk bytes.Buffer
