dccd431487ce061b30f1fa90f29d50ce728586be
kdrag0n <dragon@khronodragon.com>
2018-07-28 15:22:08 -0700

    Global cleanup

--- app/src/main/java/com/kdrag0n/tipatch/MainActivity.kt
+++ app/src/main/java/com/kdrag0n/tipatch/MainActivity.kt
@@ -3,51 +3,29 @@ package com.kdrag0n.tipatch
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.ProgressDialog
-import android.content.Context
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.kdrag0n.utils.asyncExec
-import com.kdrag0n.utils.getProp
-import eu.chainfire.libsuperuser.Shell
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var execPath: String
+    @Volatile private var libLoaded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         asyncExec {
-            try {
-                setupExecutable()
-            } catch (err: IllegalStateException) {
-                runOnUiThread {
-                    with (AlertDialog.Builder(this)) {
-                        setTitle("Oops...")
-                        setMessage(err.message)
-                        setPositiveButton(R.string.contact) { _, _ ->
-                            showContactDialog()
-                        }
-                        setNegativeButton(R.string.exit) { _, _ ->
-                            finish()
-                        }
-                        setCancelable(false)
-                        show()
-                    }
-                }
-            }
+            setupLibrary()
         }
 
         patchBtn.setOnClickListener { _ ->
-            if (!::execPath.isInitialized) {
+            if (!libLoaded) {
                 with (AlertDialog.Builder(this)) {
-                    setMessage("Tipatch has not finished setting up. Please wait a few seconds or re-open the app.")
+                    setMessage("Tipatch has not finished loading. Please wait a few seconds or re-open the app.")
                     setPositiveButton(getString(R.string.ok)) { _, _ -> }
                     show()
                 }
@@ -82,56 +60,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getArch(): String? {
-        val abi = getProp("ro.product.cpu.abi")
+    private fun setupLibrary() {
+        // TODO: catch and handle errors
+        System.loadLibrary("gojni")
 
-        return when (abi) {
-            "armeabi" -> "arm"
-            "armeabi-v7a" -> "armv7"
-            "x86" -> "x86"
-            "arm64-v8a" -> "arm64"
-            "x86_64" -> "x86_64"
-            "mips" -> "mips"
-            "mips64" -> "mips64"
-            else -> abi
-        }
-    }
-
-    private fun setupExecutable() {
-        val arch = getArch()
-
-        val res = resources.openRawResource(when (arch) {
-            "armv7" -> R.raw.tipatch_armv7
-            "arm64" -> R.raw.tipatch_arm64
-            "x86" -> R.raw.tipatch_x86
-            "x86_64" -> R.raw.tipatch_x86_64
-            else -> {
-                if (arch == null) {
-                    throw IllegalStateException("Sorry, Tipatch was unable to detect the architecture of your device. Please contact the developer for assistance.")
-                } else if ("armeabi" in arch && arch != "armeabi") { // other variants..?
-                    R.raw.tipatch_armv7
-                } else {
-                    throw IllegalStateException("Sorry, Tipatch does not currently support your architecture '$arch'. Contact the developer for help.")
-                }
-            }
-        })
-
-        val buf = ByteArray(res.available())
-        res.use {
-            it.read(buf)
-        }
-
-        openFileOutput("tipatch_bin", Context.MODE_PRIVATE).use {
-            it.write(buf)
-        }
-
-        val file = getFileStreamPath("tipatch_bin")
-        file.setExecutable(true)
-
-        execPath = file.absolutePath
+        libLoaded = true
     }
 
     private fun showContactDialog() {
         Toast.makeText(applicationContext, "TODO: contact buttons", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun errorDialog(message: String) {
+        runOnUiThread {
+            with (AlertDialog.Builder(this)) {
+                setTitle("Oops...")
+                setMessage(message)
+                setPositiveButton(R.string.contact) { _, _ ->
+                    showContactDialog()
+                }
+                setNegativeButton(R.string.exit) { _, _ ->
+                    finish()
+                }
+                setCancelable(false)
+                show()
+            }
+        }
     }
 }
