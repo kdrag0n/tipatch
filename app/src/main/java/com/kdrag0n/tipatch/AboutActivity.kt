830e23bc44f481416af32c35dd1429494b84052c
kdrag0n <dragon@khronodragon.com>
2018-08-11 11:47:34 -0700

    Revamp About screen
    Using Material About Library

--- app/src/main/java/com/kdrag0n/tipatch/AboutActivity.kt
+++ app/src/main/java/com/kdrag0n/tipatch/AboutActivity.kt
@@ -1,23 +1,81 @@
 package com.kdrag0n.tipatch
 
-import android.app.Activity
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
+import com.danielstone.materialaboutlibrary.ConvenienceBuilder as ItemBuilder
+import com.danielstone.materialaboutlibrary.MaterialAboutActivity
+import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem
+import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem
+import com.danielstone.materialaboutlibrary.model.MaterialAboutCard
+import com.danielstone.materialaboutlibrary.model.MaterialAboutList
 
-class AboutActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+class AboutActivity : MaterialAboutActivity() {
+    override fun getMaterialAboutList(ctx: Context): MaterialAboutList {
+        val appCard = MaterialAboutCard.Builder()
+                .addItem(MaterialAboutTitleItem.Builder()
+                        .text(R.string.app_name)
+                        .icon(R.mipmap.ic_launcher)
+                        .build())
+                .addItem(ItemBuilder.createVersionActionItem(ctx,
+                        getDrawable(R.drawable.ic_info_outline),
+                        "Version",
+                        false))
+                .addItem(MaterialAboutActionItem.Builder()
+                        .icon(R.drawable.ic_github)
+                        .text(R.string.about_src)
+                        .subText(R.string.about_src_desc)
+                        .setOnClickAction(ItemBuilder.createWebsiteOnClickAction(ctx, Uri.parse(R.string.source_uri())
+                        ))
+                        .build())
+                .addItem(MaterialAboutActionItem.Builder()
+                        .icon(R.drawable.ic_money)
+                        .text(R.string.donate)
+                        .subText(R.string.about_donate_desc)
+                        .setOnClickAction(ItemBuilder.createWebsiteOnClickAction(ctx, Uri.parse(R.string.donate_uri())))
+                        .build())
+                .build()
 
-        if (savedInstanceState == null) {
-            val frag = AboutFragment()
-            frag.retainInstance = true
+        val authorCard = MaterialAboutCard.Builder()
+                .title(R.string.author)
+                .addItem(MaterialAboutActionItem.Builder()
+                        .icon(R.drawable.ic_person)
+                        .text(R.string.author_name)
+                        .subText(R.string.author_nick)
+                        .build())
+                .addItem(MaterialAboutActionItem.Builder()
+                        .icon(R.drawable.ic_send)
+                        .text("Telegram")
+                        .setOnClickAction(ItemBuilder.createWebsiteOnClickAction(ctx, Uri.parse(R.string.telegram_uri())))
+                        .build())
+                .addItem(MaterialAboutActionItem.Builder()
+                        .icon(R.drawable.ic_github)
+                        .text("GitHub")
+                        .setOnClickAction(ItemBuilder.createWebsiteOnClickAction(ctx, Uri.parse(R.string.github_uri())))
+                        .build())
+                .addItem(MaterialAboutActionItem.Builder()
+                        .icon(R.drawable.ic_xda)
+                        .text("XDA-Developers")
+                        .setOnClickAction(ItemBuilder.createWebsiteOnClickAction(ctx, Uri.parse(R.string.xda_uri())))
+                        .build())
+                .addItem(MaterialAboutActionItem.Builder()
+                        .icon(R.drawable.ic_email)
+                        .text("Email")
+                        .setOnClickAction(ItemBuilder.createWebsiteOnClickAction(ctx, Uri.parse("mailto:" + R.string.contact_mail().replace(" (at) ", "@"))))
+                        .build())
+                .build()
 
-            fragmentManager
-                    .beginTransaction()
-                    .add(R.id.about_container, frag)
-                    .commit()
-        }
+        return MaterialAboutList.Builder()
+                .addCard(appCard)
+                .addCard(authorCard)
+                .build()
+    }
 
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+    override fun getActivityTitle(): CharSequence? {
+        return R.string.about()
+    }
+
+    private operator fun Int.invoke(vararg fmt: Any): String {
+        return resources.getString(this, *fmt)
     }
 }
