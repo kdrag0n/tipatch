851cb206a9edc4a44a7b03f43174a3b0b4b2cc04
kdrag0n <dragon@khronodragon.com>
2018-08-15 20:04:18 -0700

    cpp: util: byte_array: write: fix memcpy length

--- app/src/main/cpp/util.cpp
+++ app/src/main/cpp/util.cpp
@@ -16,7 +16,7 @@ void byte_array::write(const void *src, size_t src_len) {
         throw std::out_of_range("Attempting to write " + std::to_string(src_len) + " to byte array of length " + std::to_string(len) + "; however, " + std::to_string(pos - data) + " bytes have already been used.");
     }
 
-    memcpy(data, src, len);
+    memcpy(data, src, src_len);
     pos += len;
 }
 
